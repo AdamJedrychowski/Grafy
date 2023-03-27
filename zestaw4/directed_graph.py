63702275dbc61cf1965b430fc051502aeed68bbf
mmrozinski <mmrozinski@student.agh.edu.pl>
2023-03-27 19:35:24 +0200

    Add kosaraju in zestaw4

--- zestaw4/directed_graph.py
+++ zestaw4/directed_graph.py
@@ -6,6 +6,21 @@ from zestaw1.conversions import lst2adjacency, lst2incidence
 import draw
 
 
+def transpose(graph):
+    """
+    Creates a transpose version of the directed graph
+    - graph - directed graph represented as an adjacency list
+    - returns: the transpose of the provided graph
+    """
+    graph_t = {v: [] for v in graph}
+
+    for v in graph:
+        for u in graph[v]:
+            graph_t[u].append(v)
+
+    return graph_t
+
+
 if __name__ == '__main__':
     graph = randomize_lst_prob(6, 0.3, directed=True)
     print(graph, end='\n\n')
---
+++ zestaw4/kosaraju.py
@@ -0,0 +1,52 @@
+import draw
+from zestaw1.randomization import randomize_lst_prob
+from zestaw4.directed_graph import transpose
+
+
+def kosaraju(graph):
+    """
+    Calculates the graph's strongly connected components
+    - graph - directional graph represented as an adjacency list
+    - returns: dictionary in format dict[v] = n where v - the vertex index and n - id number of the component
+    """
+    d = {i: -1 for i in graph}
+    stack = []
+
+    for v in graph:
+        if d[v] == -1:
+            _DFS_visit(v, graph, d, stack)
+
+    graph_t = transpose(graph)
+    nr = 0
+
+    comp = {i: -1 for i in graph_t}
+
+    for v in stack:
+        if comp[v] == -1:
+            nr += 1
+            comp[v] = nr
+            _components_r(nr, v, graph_t, comp)
+
+    return comp
+
+
+def _DFS_visit(v, graph, d, stack):
+    d[v] = 0
+    for u in graph[v]:
+        if d[u] == -1:
+            _DFS_visit(u, graph, d, stack)
+
+    stack.append(v)
+
+
+def _components_r(nr, v, graph_t, comp):
+    for u in graph_t[v]:
+        if comp[u] == -1:
+            comp[u] = nr
+            _components_r(nr, u, graph_t, comp)
+
+
+if __name__ == '__main__':
+    graph = randomize_lst_prob(6, 0.3, directed=True)
+    print(kosaraju(graph))
+    draw.draw_graph(graph, coding=draw.Code.DIRECTED_GRAPH)
